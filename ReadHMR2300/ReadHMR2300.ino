5882e176d5ed8849125eef7c0637855af61bd247
David Rush <david.rush@wyo.gov>
2021-03-23 06:17:32 -0600

    Tried but failed to use serialEvent (not supported on MKR 1310), but improved ReadHMR2300 overall.

--- ReadHMR2300/ReadHMR2300.ino
+++ ReadHMR2300/ReadHMR2300.ino
@@ -11,16 +11,20 @@ int n = 0;
 // state 1 = command sent
 // state 2 = just do single-shot on request
 int state = 0;
+int lastState = -1;
 char buf[100];
 char receiveBuf[RECEIVE_BUF_SIZE];
 int receiveBufNextChar = 0;
 boolean waitingForOk = false;
 long waitUntilMs = 0L;
 int receiveCount = 0;
+int samplesPerSecond = 20;
+long timerStart = 0;
+long timerStop = 0;
 
 void setup() {
   // initialize serial communications and wait for port to open:
-  Serial.begin(115200); // USB and serial monitor
+  Serial.begin(19200); // USB and serial monitor
   while (!Serial) {
     ; // wait for serial port
   }
@@ -37,6 +41,11 @@ void setup() {
 
 
 void loop() {
+  if (state != lastState) {
+    Serial.print("new state = ");
+    Serial.println(state);
+    lastState = state;
+  }
   loopLED();
   loopHMR();
 }
@@ -55,8 +64,10 @@ void loopLED() {
   }
 }
 
+
 void loopHMR() {
   int data = HMR.read();
+  long a, b, elapsed;
   if (data > 0) {
     if (data == 13 || receiveBufNextChar >= RECEIVE_BUF_SIZE - 1) {
       processReceiveBuf();
@@ -67,7 +78,7 @@ void loopHMR() {
   switch(state) {
     case 0: // wait 3 seconds before starting
       if (millis() > 3000) {
-        int samplesPerSecond = 20;
+        // int samplesPerSecond = 20;
         Serial.print("Setting polling rate to ");
         Serial.println(samplesPerSecond);
         sprintf(buf, "*99R=%d\r", samplesPerSecond);
@@ -88,12 +99,14 @@ void loopHMR() {
       // poll for X/Y/Z
       // HMR.write("*99P\r");
       HMR.write("*99C\r"); // continuous until ESC
+      timerStart = millis();
       state++;
-      waitUntilMs = millis() + 100000L; // wait 10 seconds
+      waitUntilMs = millis() + 50000L; // wait 10 seconds
       break;
     case 3: // wait for waitUntilMs has passed
       if (millis() >= waitUntilMs) {
         state++;
+        timerStop = millis();
       }
       break;
     case 4:  // cancel continuous
@@ -102,41 +115,26 @@ void loopHMR() {
       Serial.write("Stopped.\n");
       state++;
       break;
-    case 5: // do nothing
+    case 5: // send test
+      /*
+      if (true) {
+        a = millis();
+        Serial.println("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567890");
+        delay(10);
+        b = millis();
+        elapsed = (int)(b - a);
+        Serial.print("send time ");
+        Serial.print(elapsed);
+        Serial.println(" ms");
+      }
+      state++;
+      */
+      break;
+    case 6: // do nothing
       break;
   }
-
-      /*
-  if (state == 0 && millis() > 3000) {
-    Serial.println("Starting continuous...");
-    HMR.write("*99R=010\r"); // request 10 Hz rate
-    state++;
-  }
-    HMR.write("*99C\r");    // request continuous sending
-    state++;
-  }
-  
-  if (state == 1 && millis() > 5000) {
-    Serial.write("\nStopping...\n");
-    HMR.write(ESC);
-    HMR.write("\n");
-    Serial.write("\nStopped.\n");
-    state++;
-  }
-  
-  if (state == 2) {
-    Serial.println("Polling...");
-    HMR.write("*99P\r"); // P for Polled
-    state++;
-  }
-  
-  if (state == 3) {
-    sprintf(buf, "state = %d\n", state);
-    Serial.print(buf);
-    state++;
-  }
-  */
 }
+
 
 void waitForResponse() {
   boolean done = false;
@@ -155,11 +153,27 @@ void waitForResponse() {
 
 void processReceiveBuf() {
   receiveBuf[receiveBufNextChar++] = (char)0; // null terminate the string
-  Serial.print("receiveBuf = ");
-  Serial.println(receiveBuf);
+  Serial.print("receiveBuf = \"");
+  Serial.print(receiveBuf);
+  Serial.println("\"");
   receiveCount++;
-  Serial.print("receiveCount = ");
-  Serial.println(receiveCount);
+  if (false) {
+    Serial.print("receiveCount = ");
+    Serial.println(receiveCount);
+  }
   receiveBufNextChar = 0;
   waitingForOk = false;
+  if (state == 5) {
+    Serial.println("yes");
+    int elapsed = (int)(timerStop - timerStart);
+    int messagesPerSec = receiveCount * 1000 / elapsed;
+    sprintf(buf, "received %d messages in %d ms or %d msg/sec, samplesPerSec = %d"
+      , receiveCount, elapsed, messagesPerSec, samplesPerSecond);
+    Serial.println(buf);
+    delay(500);
+    // sprintf(buf, "That's %d messages per second", messagesPerSec);
+    // Serial.println(buf);
+    //Serial.println("Done.");
+    state++;
+  }
 }
